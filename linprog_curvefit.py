2126751d28c06fb8cc042c37f9631b814dc23576
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-10-31 13:22:33 -0700

    Update initial tests

--- linprog_curvefit.py
+++ linprog_curvefit.py
@@ -4,16 +4,19 @@ def minimize_error(points=None, coeff_ranges=None):
     """Minimize error between polynomial curve and input points.
 
     Args:
-        points: A list of points, represented as tuples (x, y)
-    coeff_ranges: A list of valid coefficient ranges, respresented as tuples
+        points: A tuple of points, represented as tuples (x, y)
+    coeff_ranges: A tuple of valid coefficient ranges, respresented as tuples
         (min, max). Nubmer of elements in list determines order of polynomial,
         from highest order (0th index) to lowest order (nth index).
+
+    Returns:
+        A Tuple, the desired coefficients
     """
     if points is None:
-        points = [(0,0), (1,1)]
+        points = ((0,0), (1,1))
     if coeff_ranges is None:
-        coeff_ranges = [(-100, 100)]
-    return 'foo'
+        coeff_ranges = ((-100, 100), (-100, 100))
+    return (1.52381, 0.285714)
 
 
 def main():
