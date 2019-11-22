bb5926fdf6b812911e9b9c9eb0a685ac8fd63eae
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-11-22 00:37:47 -0800

    Add initial demo

--- linprog_curvefit.py
+++ linprog_curvefit.py
@@ -8,6 +8,8 @@ for a given polynomial.
 Overview:
     Objective: Sum of errors
     Subject to: Bounds on coefficients
+
+Credit: "Curve Fitting with Linear Programming", H. Swanson and R. E. D. Woolsey
 """
 
 from __future__ import absolute_import
@@ -16,25 +18,50 @@ from __future__ import print_function
 
 from ortools.linear_solver import pywraplp
 
-def optimize_linear(points=None, coeff_ranges=None):
+def demo_linear(points=None, coeff_ranges=None):
     """Optimize coefficients for a linear polynomial."""
     solver = pywraplp.Solver(
         'linear_polynomial_solver', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-    a = solver.NumVar(coeff_ranges[0][0], coeff_ranges[0][1], 'a')
-    b = solver.NumVar(coeff_ranges[1][0], coeff_ranges[1][1], 'b')
+    m = solver.NumVar(-100, 100, 'm')
+    b = solver.NumVar(-100, 100, 'b')
+    e1_plus = solver.NumVar(0, 100, 'e1_plus')
+    e1_minus = solver.NumVar(0, 100, 'e1_minus')
+    e2_plus = solver.NumVar(0, 100, 'e2_plus')
+    e2_minus = solver.NumVar(0, 100, 'e2_minus')
+    e3_plus = solver.NumVar(0, 100, 'e3_plus')
+    e3_minus = solver.NumVar(0, 100, 'e3_minus')
+    e4_plus = solver.NumVar(0, 100, 'e4_plus')
+    e4_minus = solver.NumVar(0, 100, 'e4_minus')
+    e5_plus = solver.NumVar(0, 100, 'e5_plus')
+    e5_minus = solver.NumVar(0, 100, 'e5_minus')
 
     objective = solver.Objective()
-    objective.SetCoefficient(a, points[0][0])
-    objective.SetCoefficient(b, points[1][0])
-    objective.SetOffset(sum([points[0][1], points[1][1]]))
+    objective.SetCoefficient(e1_plus, 1)
+    objective.SetCoefficient(e1_minus, 1)
+    objective.SetCoefficient(e2_plus, 1)
+    objective.SetCoefficient(e2_minus, 1)
+    objective.SetCoefficient(e3_plus, 1)
+    objective.SetCoefficient(e3_minus, 1)
+    objective.SetCoefficient(e4_plus, 1)
+    objective.SetCoefficient(e4_minus, 1)
+    objective.SetCoefficient(e5_plus, 1)
+    objective.SetCoefficient(e5_minus, 1)
     objective.SetMinimization()
+
+    solver.Add(b - e1_plus + e1_minus == 1)
+    solver.Add(m + b - e2_plus + e2_minus == 3)
+    solver.Add(2*m + b - e3_plus + e3_minus == 2)
+    solver.Add(3*m + b - e4_plus + e4_minus == 4)
+    solver.Add(4*m + b - e5_plus + e5_minus == 5)
+    print('Number of constraints: {}, Number of variables: {}'.format(
+        solver.NumConstraints(), solver.NumVariables()))
 
     solver.Solve()
     print('Solution:')
     print('Objective value =', objective.Value())
-    print('a =', a.solution_value())
+    print('m =', m.solution_value())
     print('b =', b.solution_value())
-    return a.solution_value(), b.solution_value()
+    return m.solution_value(), b.solution_value()
 
 def optimize_polynomial(points=None, coeff_ranges=None, order=None):
     """Optimize coefficients for any order polynomial."""
@@ -57,7 +84,8 @@ def minimize_error(points=None, coeff_ranges=None):
     if coeff_ranges is None:
         coeff_ranges = ((-100, 100), (-100, 100))
 
-    print(optimize_linear(points, coeff_ranges))
+    # print(optimize_polynomial(points, coeff_ranges))
+    demo_linear(points=points, coeff_ranges=coeff_ranges)
 
     return (1.52381, 0.285714)
 
