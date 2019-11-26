e0de8b45ee07b9f66bcbaa90a43d15d7237220ff
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-11-25 22:42:36 -0800

    Cleanup prints and old demos

--- linprog_curvefit.py
+++ linprog_curvefit.py
@@ -25,78 +25,6 @@ class ErrorDefinition(enum.Enum):
     SUM_ABS_DEV = enum.auto()
     SUM_MAX_DEVIATION = enum.auto()
 
-def _demo_add_objective(
-    solver, m, b, e1_plus, e1_minus, e2_plus, e2_minus, e3_plus, e3_minus,
-    e4_plus, e4_minus, e5_plus, e5_minus):
-    objective = solver.Objective()
-    objective.SetCoefficient(e1_plus, 1)
-    objective.SetCoefficient(e1_minus, 1)
-    objective.SetCoefficient(e2_plus, 1)
-    objective.SetCoefficient(e2_minus, 1)
-    objective.SetCoefficient(e3_plus, 1)
-    objective.SetCoefficient(e3_minus, 1)
-    objective.SetCoefficient(e4_plus, 1)
-    objective.SetCoefficient(e4_minus, 1)
-    objective.SetCoefficient(e5_plus, 1)
-    objective.SetCoefficient(e5_minus, 1)
-    objective.SetMinimization()
-    return objective
-
-def _demo_add_constraints(
-    solver, m, b, e1_plus, e1_minus, e2_plus, e2_minus, e3_plus, e3_minus,
-    e4_plus, e4_minus, e5_plus, e5_minus):
-    # solver.Add(b - e1_plus + e1_minus == 1) # Alternative for constraint0
-    constraint0 = solver.Constraint(1, 1)
-    constraint0.SetCoefficient(b, 1)
-    constraint0.SetCoefficient(e1_plus, -1)
-    constraint0.SetCoefficient(e1_minus, 1)
-
-    # solver.Add(m + b - e2_plus + e2_minus == 3) # Alternative constraint1
-    constraint1 = solver.Constraint(3, 3)
-    constraint1.SetCoefficient(m, 1)
-    constraint1.SetCoefficient(b, 1)
-    constraint1.SetCoefficient(e2_plus, -1)
-    constraint1.SetCoefficient(e2_minus, 1)
-
-    solver.Add(2*m + b - e3_plus + e3_minus == 2)
-    solver.Add(3*m + b - e4_plus + e4_minus == 4)
-    solver.Add(4*m + b - e5_plus + e5_minus == 5)
-
-def demo_linear(points=None, coeff_ranges=None):
-    """Optimize coefficients for a linear polynomial."""
-    solver = pywraplp.Solver(
-        'linear_polynomial_solver', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-    # m = solver.NumVar(-100, 100, 'm')
-    # b = solver.NumVar(-100, 100, 'b')
-    m = solver.NumVar(-solver.Infinity(), solver.Infinity(), 'm')
-    b = solver.NumVar(-solver.Infinity(), solver.Infinity(), 'b')
-    e1_plus = solver.NumVar(0, 100, 'e1_plus')
-    e1_minus = solver.NumVar(0, 100, 'e1_minus')
-    e2_plus = solver.NumVar(0, 100, 'e2_plus')
-    e2_minus = solver.NumVar(0, 100, 'e2_minus')
-    e3_plus = solver.NumVar(0, 100, 'e3_plus')
-    e3_minus = solver.NumVar(0, 100, 'e3_minus')
-    e4_plus = solver.NumVar(0, 100, 'e4_plus')
-    e4_minus = solver.NumVar(0, 100, 'e4_minus')
-    e5_plus = solver.NumVar(0, 100, 'e5_plus')
-    e5_minus = solver.NumVar(0, 100, 'e5_minus')
-
-    objective = _demo_add_objective(
-        solver, m, b, e1_plus, e1_minus, e2_plus, e2_minus, e3_plus, e3_minus,
-        e4_plus, e4_minus, e5_plus, e5_minus)
-
-    _demo_add_constraints(
-        solver, m, b, e1_plus, e1_minus, e2_plus, e2_minus, e3_plus, e3_minus,
-        e4_plus, e4_minus, e5_plus, e5_minus)
-    print('Number of constraints: {}, Number of variables: {}'.format(
-        solver.NumConstraints(), solver.NumVariables()))
-
-    solver.Solve()
-    print('Solution:')
-    print('Objective value =', objective.Value())
-    print('m =', m.solution_value())
-    print('b =', b.solution_value())
-    return m.solution_value(), b.solution_value()
 
 def _generate_variables(solver, points, coeff_ranges, err_max, error_def):
     """Create coefficient variables.
@@ -132,8 +60,6 @@ def _generate_variables(solver, points, coeff_ranges, err_max, error_def):
             0, err_max, 'e' + str(point_cnt + 1) + '_minus')
         variables.append(positive_err_var)
         variables.append(negative_err_var)
-        print('just added error vars "{}" and "{}"'.format(
-            positive_err_var.name(), negative_err_var.name()))
     return variables
 
 def _generate_objective_fn(
@@ -142,7 +68,6 @@ def _generate_objective_fn(
     objective = solver.Objective()
     for variable in variables[num_of_coeff:]:
         objective.SetCoefficient(variable, 1)
-        print('just set objective for "{}"'.format(variable.name()))
     return objective
 
 def _generate_constraints(solver, points, num_of_coeff, variables):
@@ -150,21 +75,15 @@ def _generate_constraints(solver, points, num_of_coeff, variables):
     for point_num, point in enumerate(points):
         # Equivalency constraint
         constraint = solver.Constraint(point[1], point[1])
-        print('generating constraint equal to: {}'.format(point[1]))
         # Resultant Coefficient terms
         for coeff_num, coeff in enumerate(variables[:num_of_coeff]):
             power = num_of_coeff - coeff_num - 1
             x_val = point[0] ** power
-            print('x_val: {}'.format(x_val))
             constraint.SetCoefficient(coeff, x_val)
-            print('just set term with coeff: {}, point[0]: {}, power: {}'.format(
-                coeff, point[0], power))
 
         # Error terms
         ex_plus = variables[num_of_coeff + 2 * point_num]
         ex_minus = variables[num_of_coeff + 2 * point_num + 1]
-        print('setting ex_plus: {}, ex_minus: {}'.format(
-            ex_plus.name(), ex_minus.name()))
         constraint.SetCoefficient(ex_plus, -1)
         constraint.SetCoefficient(ex_minus, 1)
         constraints.append(constraint)
@@ -196,46 +115,15 @@ def get_optimal_polynomial(
     variables = _generate_variables(
         solver, points, coeff_ranges, err_max=err_max,
         error_def=error_def)
-    print('generated {} variables!'.format(solver.NumVariables()))
-    print('variables: {}, each of type {}'.format(variables, type(variables[0])))
     num_of_coeff = len(coeff_ranges)
-    objective = _generate_objective_fn(solver, num_of_coeff, variables)
-    print('generated objective: {}'.format(objective))
+    _generate_objective_fn(solver, num_of_coeff, variables)
     _generate_constraints(solver, points, num_of_coeff, variables)
-    print('generated {} constraints!'.format(solver.NumConstraints()))
-    print('variables are still: {}'.format(variables))
     solver.Solve()
-    print('solved linear program with {} coefficients!'.format(num_of_coeff))
-    print('variables are still: {}'.format(variables[:num_of_coeff]))
-    print('m: {}, b: {}'.format(
-        variables[0].solution_value(), variables[1].solution_value()))
+
     var_to_val = dict()
     for coeff in variables[:num_of_coeff]:
         var_to_val[coeff.name()] = coeff.solution_value()
     return var_to_val
-
-def minimize_error(points=None, coeff_ranges=None):
-    """Minimize error between polynomial curve and input points.
-
-    Args:
-        points: A tuple of points, represented as tuples (x, y)
-    coeff_ranges: A tuple of valid coefficient ranges, respresented as tuples
-        (min, max). Nubmer of elements in list determines order of polynomial,
-        from highest order (0th index) to lowest order (nth index).
-
-    Returns:
-        A Tuple, the desired coefficients. Ordered from highest order polynomial
-            term to lowest order.
-    """
-    if points is None:
-        points = ((0,0), (1,1))
-    if coeff_ranges is None:
-        coeff_ranges = ((-100, 100), (-100, 100))
-
-    # print(get_optimal_polynomial(points, coeff_ranges))
-    demo_linear(points=points, coeff_ranges=coeff_ranges)
-
-    return (1.52381, 0.285714)
 
 def demo_optimal_linear_5points():
     """Demonstration of getting optimal linear polynomial.
@@ -293,8 +181,7 @@ def demo_optimal_cubic_10points():
     print(get_optimal_polynomial(points=points, coeff_ranges=coeff_ranges))
 
 def main():
-    minimize_error()
-    demo_optimal_cubic_10points()
+    demo_optimal_quadratic_19points()
 
 if __name__ == '__main__':
     main()
--- test_linprog_curvefit.py
+++ test_linprog_curvefit.py
@@ -8,15 +8,6 @@ class TestLinprogCurvefit(unittest.TestCase):
         self.minimize_error = linprog_curvefit.minimize_error
         self.generate_variables = linprog_curvefit._generate_variables
 
-    def test_minimize_error_LinearFit(self):
-        """https://www.wolframalpha.com/input/?i=linear+fit+%280%2C0%29%2C+%281.5%2C+3%29%2C+%284.5%2C+7%29"""
-        points = ((0,0), (1.5, 3), (4.5, 7))
-        coeff_ranges = ((-10, 10), (-10, 10))
-        desired_coeff = (1.52381, 0.285714)
-        self.assertAlmostEqual(
-            self.minimize_error(points=points, coeff_ranges=coeff_ranges),
-            desired_coeff, 3)
-
     def test_generate_variables_2PointsLinearCorrectVarCnt(self):
         points = ((0, 0), (1.5, 3))
         coeff_ranges = ((-10, 10), (-10, 10))
